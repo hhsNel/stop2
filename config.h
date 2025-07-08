bc4835b5127de72ec6bc4c2f6f151c0bd521a6dd
hhsNel <hihijsonel@gmail.com>
2025-07-08 23:08:29 +0200

    multiple dividers proof of concept

--- config.h
+++ config.h
@@ -4,7 +4,7 @@ struct dspelement gate_menu[];
 struct dspelement main_menu[];
 #define HELP_LEN 2
 #define GATE_LEN 1
-#define MAIN_LEN 10
+#define MAIN_LEN 15
 
 /* user-defined menu */
 struct dspelement help_menu[] = {
@@ -26,11 +26,14 @@ struct dspelement main_menu[] = {
 		{EL_MENU_BUTTON,    {.v="menu btn",.i=HELP_LEN},  help_menu,     {.color_mode=CLRM_USR,.clr=5},      {.color_mode=CLRM_USR,.clr=3}},
 		{EL_MENU_BUTTON,    {.v="gate",.i=GATE_LEN},      gate_menu,     {.color_mode=CLRM_USR,.clr=5},      {.color_mode=CLRM_USR,.clr=3}},
 	{EL_GRAPH_OVER_TIME,{0},                              vcpu_load,     {.color_mode=CLRM_USR,.clr=7},      {.color_mode=CLRM_USR,.clr=0}},
-	{EL_DIVIDER,        {.i=4},                           NULL,          {},                                 {}},
+	{EL_DIVIDER,        {.i=5},                           NULL,          {},                                 {}},
 		{EL_VALUE,      {.v="%d"},                        vram_total,    {.color_mode=CLRM_USR,.clr=4},      {.color_mode=CLRM_USR,.clr=0}},
 		{EL_PLAINTEXT,  {.v="out of"},                    NULL,          {.color_mode=CLRM_USR,.clr=4},      {.color_mode=CLRM_USR,.clr=0}},
 		{EL_VALUE,      {.v="%d"},                        vram_max,      {.color_mode=CLRM_USR,.clr=4},      {.color_mode=CLRM_USR,.clr=0}},
 		{EL_PLAINTEXT,  {.v="bytes"},                     NULL,          {.color_mode=CLRM_USR,.clr=4},      {.color_mode=CLRM_USR,.clr=0}},
+		{EL_DIVIDER,    {.i=2},                           NULL,          {},                                 {}},
+			{EL_PLAINTEXT,{.v="This is"},                 NULL,          {.color_mode=CLRM_USR,.clr=6},      {.color_mode=CLRM_USR,.clr=0}},
+			{EL_PLAINTEXT,{.v="stacked text"},            NULL,          {.color_mode=CLRM_USR,.clr=6},      {.color_mode=CLRM_USR,.clr=0}},
 	{EL_PLAINTEXT,      {.v="sTop poc v0.0.1"},           NULL,          {.color_mode=CLRM_USR,.clr=1},      {.color_mode=CLRM_USR,.clr=0}},
 	{EL_H_CAP,          {.i=1},                           NULL,          {},                                 {}},
 		{EL_PLAINTEXT,  {.v="This is only one line"},     NULL,          {.color_mode=CLRM_USR,.clr=7},      {.color_mode=CLRM_USR,.clr=0}},
